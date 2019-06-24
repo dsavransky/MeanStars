e1ecbc7451acc800a962dea609737902e92ae773
Dmitry Savransky <dsavransky@gmail.com>
2019-06-23 17:22:05 -0700

    updating input file to version 2019.3.22, adding single-sourced version string in init and updating setup accordingly

--- MeanStars/__init__.py
+++ MeanStars/__init__.py
@@ -1,3 +1,4 @@
 name = "MeanStars"
+__version__ = '1.2.0'
 #from MeanStars import MeanStars
 from .MeanStars import MeanStars
--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 import setuptools
+import os, re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+with open(os.path.join("MeanStars","__init__.py"), "r") as f:
+    version_file = f.read()
+
+version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",\
+        version_file, re.M)
+
+if version_match:
+    version_string = version_match.group(1)
+else:
+    raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name="MeanStars",
-    version="1.1.1",
+    version=version_string,
     author="Dmitry Savransky",
     author_email="ds264@cornell.edu",
     description="Automated property interpolation and color calculations for main sequence stars",
