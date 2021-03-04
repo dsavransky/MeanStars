1c291e4af69cbe2297c31f2ea1eb4c38859469d7
Dmitry Savransky <dsavransky@gmail.com>
2021-03-04 11:27:28 -0500

    bumping to v3.0.0, deprecating python 2.7 support and below 3.6 support, setting up automated release action to push to pypi, and updating Mamajek table to Version 2021.03.02

--- MeanStars/__init__.py
+++ MeanStars/__init__.py
@@ -1,4 +1,4 @@
 name = "MeanStars"
-__version__ = '2.0.0'
+__version__ = '3.0.0'
 #from MeanStars import MeanStars
 from .MeanStars import MeanStars
--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setuptools.setup(
           'astropy'
     ],
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
